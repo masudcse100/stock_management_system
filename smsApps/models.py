82ff6670d487c77ea4e6be51ddc453253f4be499
masudpy <masudcse74@gmail.com>
2022-07-29 13:12:12 +0600

    categoey add

--- smsApps/models.py
+++ smsApps/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+# from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+
+class Categorie(models.Model):
+    active = 1
+    inactive = 0
+    status = (
+        (active, ('Active')),
+        (inactive, ('Inactive')),
+    )
+
+    active  = models.IntegerField(default=0, choices=status)
+    cat_name = models.CharField(max_length=264)
+    cat_brand = models.CharField(max_length=264)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        # ordering = {'-publish_date',}
+        ordering = ['-publish_date',]
+    def __str__(self):
+        return self.cat_name
