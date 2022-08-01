cf03b6e85c4c346a7c4f1ad32361a8f2b0b7a01b
masudpy <masudcse74@gmail.com>
2022-08-01 16:45:31 +0600

    paginator update

--- smsApps/urls.py
+++ smsApps/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('categorey/', views.categoreyView, name='categorey'),
     path('addcategories/', views.addCategorey, name='addcategorey'),
-
+    # path('save_category/',views.save_category,name='save-category'),
     # path('admin/', admin.site.urls),
 ]
--- smsApps/views.py
+++ smsApps/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, HttpResponseRedirect
+from requests import request
 from . forms import SignUpForm,CatForm
 from django.contrib import messages
 from . models import Categorie
 from django.urls import reverse_lazy, reverse
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
@@ -11,13 +13,18 @@ def index(request):
 
 @login_required
 def index(request):
-	cat_count = Categorie.objects.all().count()
-	return render(request, 'smsApps/dashboard.html',{'cat_count':cat_count})
+	categories_count = Categorie.objects.all().count()
+	# product_count = Categorie.objects.all().count()
+	# cat_count = Categorie.objects.all().count()
+	return render(request, 'smsApps/dashboard.html',{'cat_count':categories_count})
 
 @login_required
 def categoreyView(request):
-	allcatn = Categorie.objects.all()
-	return render(request, 'smsApps/categories.html',{'allcats':allcatn})
+    allcats = Categorie.objects.all().order_by('cat_name')
+    pagntr = Paginator(allcats, 5)
+    page_num = request.GET.get('cat_page')
+    page_obj = pagntr.get_page(page_num)
+    return render(request, 'smsApps/categories.html',{'page_cat_obj':page_obj})
 
 def addCategorey(request):
     if request.user.is_authenticated:
@@ -26,9 +33,10 @@ def addCategorey(request):
             if form.is_valid():
                 cat_name = form.cleaned_data['cat_name']
                 cat_brand = form.cleaned_data['cat_brand']
-                active = form.cleaned_data['active']
+                status = form.cleaned_data['status']
 				# active = form
-                post = Categorie(cat_name=cat_name, cat_brand=cat_brand, active=active)
+                # post = Categorie(cat_name=cat_name, cat_brand=cat_brand, active=active)
+                post = Categorie(cat_name=cat_name, cat_brand=cat_brand, status=status)
                 post.save()
                 messages.success(request,'Add post successfull!')
                 form = CatForm()
@@ -39,3 +47,5 @@ def addCategorey(request):
     else:
         return HttpResponseRedirect('/login/')
 
+# def save_category(request):
+#     return render(request, 'smsApps/modaltest.html')
